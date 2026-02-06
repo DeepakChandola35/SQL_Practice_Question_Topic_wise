18b63286117da0509ab990247f2afa26b5c429d2
Deepak Chandola <deepakchandola35@gmail.com>
2026-02-06 23:16:32 +0530

    Added window function practice queries

--- Window_function_practice_question.sql
+++ Window_function_practice_question.sql
@@ -1,1 +1,14 @@
-select * from orders
+Q: Find users whose total spend is above average spend of all users.
+
+SELECT user_id
+FROM orders
+GROUP BY user_id
+HAVING SUM(order_amount) >
+(
+  SELECT AVG(total_spend)
+  FROM (
+    SELECT SUM(order_amount) AS total_spend
+    FROM orders
+    GROUP BY user_id
+  ) t
+);
